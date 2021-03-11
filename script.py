4ec1337ce5bfdc7f34a8442d6e4ec400c3532f00
gyai <79432823+gyai@users.noreply.github.com>
2021-03-12 04:19:00 +0900

    csvデータを3次元配列で取得できるようにした

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg                                 # インポート
 from tkinter import *
 from tkinter import ttk
+import csv
 '''
 #データ取得
  ストレージの.csvファイルを開いて中身を取得
@@ -14,6 +15,15 @@ from tkinter import ttk
 '''
 ''' ここからデータ取得 '''
 
+csv_file = open("pointer_tra.csv", "r", errors="", newline="" )
+#リスト形式
+f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
+pointerlist = []
+for row in f: #全行分回す
+    #rowはList
+    #row[0]で必要な項目を取得することができる
+    pointerlist.append(row) #2次元配列pointerlist
+
 
 '''ここから見た目'''
 # ウィンドウの内容を定義する
@@ -21,7 +31,7 @@ from tkinter import ttk
 layout = [ ]    # レイアウト->ウィンドウに設定するやつ
 width = 1080  #nexus5の画面ピクセル比率(1080,1920)のまま縮小
 height = 1920
-bairitu = 1/3 #1/4倍にしている
+bairitu = 1/3 #1/3倍にしている
 
 #button作成
 # ボタンを縦7横5=35個配置
@@ -35,7 +45,7 @@ for j in range(7):
         button = sg.Button(
             button_text=button_name,
             image_filename='button_image.png',
-            image_subsample=3,
+            image_subsample=3, #1/3にしている
             #image_size=(500, 500), #width,height
             key= "button_"+str(j)+"_"+str(i)
         )
@@ -43,17 +53,50 @@ for j in range(7):
 
 
 # ウィンドウを作成する
-
 window = sg.Window('ウィンドウタイトル', layout, size=(int(width*bairitu), int(height*bairitu))) # ウィンドウ定義（サイズとか）
        
-# ウィンドウを表示し、対話する
-'''window.read()はinput()のように入力待ちの状態です。
-入力を受け取るまではこの行でずっと待機していて、
-イベントが発生したら(key, value)のタプル形式でイベントを受け取ります'''
-while True:
-    event, values = window.read()  #  イベントループまたは Window.read 呼び出し
-    print(event)
-# 収集された情報で何かをする
+
+while True: #リスナーの役割をしているらしい。何かのイベントが起きたらここの中の処理が動くように実装する
+
+    '''window.read()はinput()のように入力待ちの状態です。
+    入力を受け取るまではこの行でずっと待機していて、
+    イベントが発生したら(key, value)のタプル形式でイベントを受け取ります'''
+    event, values = window.read()  #  イベントループまたは Window.read 呼び出し->.readで[イベントが発生したよ]という情報をevent変数に伝えている
+    
+    if event.startswith("button"): #(今回の場合はevent)keyがbuttonから始まるなら->buttonを押した時の処理
+        _,y,x = event.split("_") #eventを_区切りで分割 右側がlistになる　変数yとxに押されたボタンのindex的なものが入る
+
+        y = int(y)*150
+        x = int(x)*150
+        print("button座標",int(y),int(x))
+        p_tra_array = [] 
+        splitarray = []
+        for plist in pointerlist: #リストの長さ分繰り返す。要素の一つめと"y""x"が一致している行だけ抜き出す
+            if plist[0] == (str(y)+" , "+str(x)): #リストの0要素目(ターゲット座標)がボタンと同じ行だけp_tra_arrayに抜き出した
+                p_tra_array.append(plist) ###この時点で、押したボタンの座標に対応した行(30行分)だけ入ってる
+                sarray = []
+                for pl in plist[2:]: #plistの3番目から(スライス)
+                    #del pl[0],pl[1] ##ターゲット座標とポインター座標のテキストの要素を削除
+                    if " , "in pl:
+                        sarray.append(pl.split(" , ")) ##splitarrayの1次元目が[0]~[29]行情報、2次元目の0=x座標,1=y座標
+                splitarray.append(sarray)
+
+                    
+        print(splitarray[5]) #splitarray[行][列][x座標 or y座標]が出力できる
+        '''
+        ・押されたbutton以外は非表示にしたい
+        ・画面下にリセットボタン(非表示などを初期状態に戻す)
+        ・一行ずつ表示できるように仕様変えるべきかも？？？(後回し)
+       ----- ここに処理を書く-----
+
+        yとxに押されたbuttonの識別子が入っているから、各行の最初の文字と比較することで対応する軌跡の行を抽出できる(はず)
+        30行分抽出できたら","で区切って、各行の要素を取り出してintにキャスト→直線引く用のlistにいれる
+        直線用listの要素数分直線を引けば軌跡が出るはず->1/3にするの忘れない
+        各タスクの軌跡は色か太さで区別
+        '''
+        
+
+
 
 
 # 画面から削除して終了
