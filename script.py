7b51dbc49a8dc79b8d310877b0ca464ff584fbc6
gyai <79432823+gyai@users.noreply.github.com>
2021-03-13 06:27:20 +0900

    完成

--- script.py
+++ script.py
@@ -39,7 +39,7 @@ class MainDisplay:
 
         '''ここから見た目'''
         # ウィンドウの内容を定義する
-        #画面上部に縦7x横5=35個のbutton配置(150*150)
+        #画面上部に縦5x横7=35個のbutton配置(150*150)
 
         self.layout = [ ]    # レイアウト->ウィンドウに設定するやつ
         #column = [ ]
@@ -48,21 +48,21 @@ class MainDisplay:
         self.bairitu = 1/3 #1/3倍にしている
 
         #button作成
-        # ボタンを縦7横5=35個配置
-        for j in range(7):
+        # ボタンを縦5横7=35個配置
+        for j in range(5):
             #column.append([])
             self.layout.append([])
-            for i in range(5):
+            for i in range(7):
                 # ボタンの名前（テキスト）を設定
-                button_name = "(" + str(j) + "," + str(i)+ ")"
+                button_name = "(" + str(i) + "," + str(j)+ ")"
 
                 # ボタンのインスタンス作成
                 button = sg.Button(
                     button_text=button_name,
                     image_filename='button_image.png',
-                    image_subsample=3, #1/3にしている
-                    #image_size=(500, 500), #width,height
-                    key= "button_"+str(j)+"_"+str(i)
+                    image_subsample=5, #1/3にしている
+                    image_size=(500, 500), #width,height
+                    key= "button_"+str(i)+"_"+str(j)
                 )
                 #column[j].append(button)
                 self.layout[j].append(button)
@@ -85,10 +85,10 @@ class MainDisplay:
             入力を受け取るまではこの行でずっと待機していて、
             イベントが発生したら(key, value)のタプル形式でイベントを受け取ります'''
             event, values = self.window.read()  #  イベントループまたは Window.read 呼び出し->.readで[イベントが発生したよ]という情報をevent変数に伝えている
-            #print(event,values) #eventが'button_0_0'。valuesはよくわからんけど'{}'が出力
+            print(event) #eventが'button_0_0'。valuesはよくわからんけど'{}'が出力
 
             if event.startswith("button"): #(今回の場合はevent)keyがbuttonから始まるなら->buttonを押した時の処理
-                _,y,x = event.split("_") #eventを_区切りで分割 右側がlistになる　変数yとxに押されたボタンのindex的なものが入る
+                _,x,y = event.split("_") #eventを_区切りで分割 右側がlistになる　変数yとxに押されたボタンのindex的なものが入る
 
                 #押されたbuttonの情報取得
                 y = int(y)*150
@@ -98,11 +98,10 @@ class MainDisplay:
                 self.p_tra_array = [] 
                 self.splitarray = []
                 for plist in self.pointerlist: #リストの長さ分繰り返す。要素の一つめと"y""x"が一致している行だけ抜き出す
-                    if plist[0] == (str(y)+" , "+str(x)): #リストの0要素目(ターゲット座標)がボタンと同じ行だけp_tra_arrayに抜き出した
+                    if plist[0] == (str(x)+" , "+str(y)): #リストの0要素目(ターゲット座標)がボタンと同じ行だけp_tra_arrayに抜き出した
                         self.p_tra_array.append(plist) ###この時点で、押したボタンの座標に対応した行(30行分)だけ入ってる
                         sarray = []
                         for pl in plist[2:]: #plistの3番目から(スライス)
-                            #del pl[0],pl[1] ##ターゲット座標とポインター座標のテキストの要素を削除
                             if " , "in pl:
                                 sarray.append(pl.split(" , ")) ##splitarrayの1次元目が[0]~[29]行情報、2次元目の0=x座標,1=y座標
                         self.splitarray.append(sarray)
@@ -154,35 +153,33 @@ class SubDisplay:
         self.subwindow.finalize()  
         self.count = 0
         #ここにMainクラスで作成した座標リストを入れる
-
+        print(self.maindis.splitarray[0])
         for i, slist in enumerate(self.maindis.splitarray):
             #splitarrayの各行の長さ(ポインター座標の数)をcountで取得
             code = '#'+''.join(list(map(lambda x: '{:02x}'.format(int(x * 255)), colorsys.hsv_to_rgb(i/len(self.maindis.splitarray) , 1, 1))))
             for c in range(len(slist)-1):
                 #print(int(float(slist[c][0])*self.maindis.bairitu))
-                print(len(slist)-3)
                 #print(type(float(int(slist[c][0])*self.maindis.bairitu)))
                 self.trajectory = self.canvas.TKCanvas.create_line(
                     int(float(slist[c][0])*self.maindis.bairitu),
-                    int(float(slist[c][1])), 
+                    int(float(slist[c][1])*self.maindis.bairitu), 
                     int(float(slist[c+1][0])*self.maindis.bairitu), 
-                    int(float(slist[c+1][1]))
+                    int(float(slist[c+1][1])*self.maindis.bairitu)
                 )
                 if c==0:
                     self.startpoint = self.canvas.TKCanvas.create_rectangle(
                         int(float(slist[c][0])*self.maindis.bairitu),
-                        int(float(slist[c][1])), 
+                        int(float(slist[c][1])*self.maindis.bairitu), 
                         int(float(slist[c][0])*self.maindis.bairitu)+5,
-                        int(float(slist[c][1]))+5 
+                        int(float(slist[c][1])*self.maindis.bairitu)+5 
                     )
                     self.canvas.TKCanvas.itemconfig(self.startpoint, fill=code)  #各行をグラデーションで表示したい
                 if c==int(len(slist)-3):
-                    print('入った')
                     self.endpoint = self.canvas.TKCanvas.create_oval(
                         int(float(slist[c+1][0])*self.maindis.bairitu),
-                        int(float(slist[c+1][1])), 
+                        int(float(slist[c+1][1])*self.maindis.bairitu), 
                         int(float(slist[c+1][0])*self.maindis.bairitu)+5,
-                        int(float(slist[c+1][1]))+5 
+                        int(float(slist[c+1][1])*self.maindis.bairitu)+5 
                     )
                 self.canvas.TKCanvas.itemconfig(self.trajectory, fill=code)  #各行をグラデーションで表示したい
             self.canvas.TKCanvas.itemconfig(self.endpoint, fill=code)  #各行をグラデーションで表示したい
@@ -203,7 +200,6 @@ class SubDisplay:
                         code = '#'+''.join(list(map(lambda x: '{:02x}'.format(int(x * 255)), colorsys.hsv_to_rgb(i/len(self.maindis.splitarray) , 1, 1))))
                         for c in range(len(slist)-1):
                             #print(int(float(slist[c][0])*self.maindis.bairitu))
-                            print(len(slist)-3)
                             #print(type(float(int(slist[c][0])*self.maindis.bairitu)))
                             self.trajectory = self.canvas.TKCanvas.create_line(
                                 int(float(slist[c][0])*self.maindis.bairitu),
@@ -219,7 +215,6 @@ class SubDisplay:
                                 )
                                 self.canvas.TKCanvas.itemconfig(self.startpoint, fill=code)  #各行をグラデーションで表示したい
                             if c==int(len(slist)-3):
-                                print('入った')
                                 self.endpoint = self.canvas.TKCanvas.create_oval(
                                     int(float(slist[c+1][0])*self.maindis.bairitu),
                                     int(float(slist[c+1][1])), 
@@ -231,8 +226,5 @@ class SubDisplay:
         self.subwindow.close()
 
 
-#-------class1を実行するために書かないといけないらしい???--------
-
-###ここが悪いのか？？？→2画面出るようになったけど、1回目のクリックしか反応しない。ループできるようにするべきなのか？？
 disp1 = MainDisplay()
 disp1.main()
